0a93a84c64e02527d6a1b8bf804690bd9dce7617
Newton Ni <nwtnni@gmail.com>
2018-12-02 00:08:08 -0500

    Implement 2015 day 6

---
+++ 2015/day-6/src/one.rs
@@ -0,0 +1,26 @@
+extern crate day_6;
+
+use day_6::*;
+
+const INPUT: &'static str = include_str!("input.txt");
+
+fn main() {
+
+    let mut grid = vec![vec![false; 1000]; 1000];
+    
+    for insn in INPUT.trim().split('\n').map(Instruction::parse) {
+        for pos in insn.start.to(insn.end) {
+            grid[pos.y][pos.x] = match insn.mode {
+            | Mode::On => true,
+            | Mode::Off => false,
+            | Mode::Toggle => !grid[pos.y][pos.x],
+            };
+        }
+    }
+
+    let lit = grid.into_iter()
+        .map(|row| row.into_iter().filter(|light| *light).count())
+        .sum::<usize>();
+
+    println!("{}", lit);
+}
