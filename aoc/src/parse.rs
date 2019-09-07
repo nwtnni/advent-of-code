edf1baada611134b866b84ea872bd3877220069c
Newton Ni <nwtnni@gmail.com>
2019-09-07 15:51:51 -0400

    Refactor parsing

---
+++ aoc/src/parse.rs
@@ -0,0 +1,68 @@
+use std::str;
+
+impl str::FromStr for crate::Day {
+    type Err = crate::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use crate::Day::*;
+        match s.parse::<u8>().map_err(crate::Error::InvalidInt)? {
+        | 01 => Ok(D01),
+        | 02 => Ok(D02),
+        | 03 => Ok(D03),
+        | 04 => Ok(D04),
+        | 05 => Ok(D05),
+        | 06 => Ok(D06),
+        | 07 => Ok(D07),
+        | 08 => Ok(D08),
+        | 09 => Ok(D09),
+        | 10 => Ok(D10),
+        | 11 => Ok(D11),
+        | 12 => Ok(D12),
+        | 13 => Ok(D13),
+        | 14 => Ok(D14),
+        | 15 => Ok(D15),
+        | 16 => Ok(D16),
+        | 17 => Ok(D17),
+        | 18 => Ok(D18),
+        | 19 => Ok(D19),
+        | 20 => Ok(D20),
+        | 21 => Ok(D21),
+        | 22 => Ok(D22),
+        | 23 => Ok(D23),
+        | 24 => Ok(D24),
+        | 25 => Ok(D25),
+        | _ => Err(crate::Error::InvalidDay(s.to_string())),
+        }
+    }
+}
+
+impl str::FromStr for crate::Part {
+    type Err = crate::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use crate::Part::*;
+        match s.parse::<u8>().map_err(crate::Error::InvalidInt)? {
+        | 1 => Ok(P01),
+        | 2 => Ok(P02),
+        | _ => Err(crate::Error::InvalidPart(s.to_string())),
+        }
+    }
+}
+
+impl str::FromStr for crate::Digit {
+    type Err = crate::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use crate::Digit::*;
+        match s.parse::<u8>().map_err(crate::Error::InvalidInt)? {
+        | 0 => Ok(D0),
+        | 1 => Ok(D1),
+        | 2 => Ok(D2),
+        | 3 => Ok(D3),
+        | 4 => Ok(D4),
+        | 5 => Ok(D5),
+        | 6 => Ok(D6),
+        | 7 => Ok(D7),
+        | 8 => Ok(D8),
+        | 9 => Ok(D9),
+        | _ => Err(crate::Error::InvalidDigit(s.to_string()))
+        }
+    }
+}
---
+++ aoc/src/types.rs
@@ -0,0 +1,80 @@
+use std::num;
+use std::str;
+
+#[derive(Debug, failure::Fail)]
+pub enum Error {
+    #[fail(display = "invalid integer: {}", _0)]
+    InvalidInt(#[fail(cause)] num::ParseIntError),
+
+    #[fail(display = "invalid day: {}", _0)]
+    InvalidDay(String),
+
+    #[fail(display = "invalid part: {}", _0)]
+    InvalidPart(String),
+    
+    #[fail(display = "invalid digit: {}", _0)]
+    InvalidDigit(String),
+}
+
+#[repr(u8)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Digit {
+    D0 = 0,
+    D1 = 1,
+    D2 = 2,
+    D3 = 3,
+    D4 = 4,
+    D5 = 5,
+    D6 = 6,
+    D7 = 7,
+    D8 = 8,
+    D9 = 9,
+}
+
+#[repr(u8)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Day {
+    D01 = 01, 
+    D02 = 02, 
+    D03 = 03, 
+    D04 = 04, 
+    D05 = 05, 
+    D06 = 06, 
+    D07 = 07, 
+    D08 = 08, 
+    D09 = 09, 
+    D10 = 10, 
+    D11 = 11, 
+    D12 = 12, 
+    D13 = 13, 
+    D14 = 14, 
+    D15 = 15, 
+    D16 = 16, 
+    D17 = 17, 
+    D18 = 18, 
+    D19 = 19, 
+    D20 = 20, 
+    D21 = 21, 
+    D22 = 22, 
+    D23 = 23, 
+    D24 = 24, 
+    D25 = 25, 
+}
+
+#[repr(u8)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Part {
+    P01 = 1,
+    P02 = 2,
+}
+
+pub trait Solution: str::FromStr {
+    fn one(self) -> usize;
+    fn two(self) -> usize;
+    fn solve(self, part: Part) -> usize {
+        match part {
+        | Part::P01 => self.one(),
+        | Part::P02 => self.two(),
+        }
+    }
+}
