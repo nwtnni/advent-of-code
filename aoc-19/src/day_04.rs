6772c6824fe345b0be3e253de07e40cb4cd3c033
Newton Ni <nwtnni@gmail.com>
2019-12-04 00:24:45 -0500

    Clean up 2019 D4 implementation
    - Use modulo/divide instead of string parsing
    - Use descriptive variable names
    - Reuse HashSet instead of re-allocating

--- aoc-19/src/day_04.rs
+++ aoc-19/src/day_04.rs
@@ -2,66 +2,90 @@ use std::collections::HashSet;
 use std::str;
 
 #[derive(Clone, Debug)]
-pub struct Placeholder(i32, i32);
+pub struct SecureContainer {
+    lo: i32,
+    hi: i32,
+}
 
-impl str::FromStr for Placeholder {
+impl str::FromStr for SecureContainer {
     type Err = aoc::Error;
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         let mut iter = input.trim().split('-');
-        let l = iter
+        let lo = iter
             .next()
             .unwrap()
             .parse::<i32>()
-            .unwrap();
-        let r = iter
+            .map_err(aoc::Error::InvalidInt)?;
+        let hi = iter
             .next()
             .unwrap()
             .parse::<i32>()
-            .unwrap();
-        Ok(Placeholder(l, r))
+            .map_err(aoc::Error::InvalidInt)?;
+        Ok(SecureContainer {
+            lo,
+            hi,
+        })
     }
 }
 
-impl aoc::Solution for Placeholder {
+impl aoc::Solution for SecureContainer {
     fn one(self) -> i32 {
+
         let mut count = 0;
-        'outer: for i in self.0..self.1 {
-            let s = i.to_string();
-            let mut last = 0;
+
+        'outer: for i in self.lo..self.hi {
+            let mut prev = None;
             let mut double = false;
-            for j in 0..s.len() {
-                let x = s[j..j+1].parse::<i32>().unwrap();
-                if x < last { continue 'outer; }
-                double |= last == x;
-                last = x;
+            let mut number = i;
+
+            for place in &[100000, 10000, 1000, 100, 10, 1] {
+                let next = number / place;
+                number %= place;
+
+                if next < prev.unwrap_or(-1) { continue 'outer }
+
+                double |= prev == Some(next);
+                prev = Some(next);
             }
+
             if double {
-            count += 1;
+                count += 1;
             }
         }
+
         count
     }
 
     fn two(self) -> i32 {
+
         let mut count = 0;
-        'outer: for i in self.0..self.1 {
-            let s = i.to_string();
-            let mut last = None;
-            let mut last_2 = None;
-            let mut double = HashSet::new();
-            let mut triple = HashSet::new();
-            for j in 0..s.len() {
-                let x = s[j..j+1].parse::<i32>().unwrap();
-                if x < last.unwrap_or(-1) { continue 'outer; }
-                if last == Some(x) {
-                    double.insert(x);
+        let mut double = HashSet::new();
+        let mut triple = HashSet::new();
+
+        'outer: for i in self.lo..self.hi {
+            double.clear();
+            triple.clear();
+
+            let mut prev = [None; 2];
+            let mut number = i;
+
+            for place in &[100000, 10000, 1000, 100, 10, 1] {
+                let next = number / place;
+                number %= place;
+
+                if next < prev[0].unwrap_or(-1) { continue 'outer; }
+
+                if Some(next) == prev[0] {
+                    double.insert(next);
+                    if Some(next) == prev[1] {
+                        triple.insert(next);
+                    }
                 }
-                if Some(x) == last && Some(x) == last_2 {
-                    triple.insert(x);
-                }
-                last_2 = last;
-                last = Some(x);
+
+                prev[1] = prev[0];
+                prev[0] = Some(next);
             }
+
             if double.difference(&triple).count() > 0 {
                 count += 1;
             }
--- aoc-19/src/lib.rs
+++ aoc-19/src/lib.rs
@@ -13,7 +13,7 @@ pub fn solve(day: aoc::Day, part: aoc::Part, input: &str) -> Result<i32, aoc::Er
     | aoc::Day::D01 => run!(day_01::TheTyrannyOfTheRocketEquation),
     | aoc::Day::D02 => run!(day_02::ProgramAlarm),
     | aoc::Day::D03 => run!(day_03::CrossedWires),
-    | aoc::Day::D04 => run!(day_04::Placeholder),
+    | aoc::Day::D04 => run!(day_04::SecureContainer),
     | _ => unimplemented!(),
     }
 }
