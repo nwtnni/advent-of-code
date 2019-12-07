e2104243c3f41b6b7b98e2e11b279e6702ccb5ef
Newton Ni <nwtnni@gmail.com>
2019-12-07 10:00:17 -0500

    Bound permutation length to u8
    Because we only emit swaps and lazily evaluate, we're technically
    bounded by memory usage, not runtime. But since `255!` already
    has 158 digits, it seems reasonable to assume that nobody should
    attempt to permute very large lengths.

--- aoc-core/src/util.rs
+++ aoc-core/src/util.rs
@@ -8,7 +8,7 @@ pub fn snd<A, B>((_, b): (A, B)) -> B {
     b
 }
 
-pub fn permute(len: usize) -> impl Iterator<Item = (usize, usize)> {
+pub fn permute(len: u8) -> impl Iterator<Item = (usize, usize)> {
     iter::once((0, 0)).chain(Permutations::new(len))
 }
 
@@ -16,7 +16,7 @@ pub fn permute(len: usize) -> impl Iterator<Item = (usize, usize)> {
 enum Dir { P, Z, N, }
 
 impl Dir {
-    fn step(&self, start: usize) -> usize {
+    fn step(&self, start: u8) -> u8 {
         match self {
         | Dir::P => start + 1,
         | Dir::Z => start,
@@ -27,7 +27,7 @@ impl Dir {
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 struct Info {
-    pos: usize,
+    pos: u8,
     dir: Dir,
 }
 
@@ -37,27 +37,27 @@ struct Info {
 #[derive(Clone, Debug)]
 pub struct Permutations {
     /// Upper bound on indices
-    bound: usize,
+    bound: u8,
 
     /// Completed permutations
-    count: usize,
+    count: u8,
 
     /// Current permutation as indices
-    data: Vec<usize>,
+    data: Vec<u8>,
 
     /// Position and direction of each index
     info: Vec<Info>,
 }
 
 impl Permutations {
-    fn new(len: usize) -> Self {
+    fn new(len: u8) -> Self {
         let data = (0..len).collect::<Vec<_>>();
         let mut info = (0..len)
             .map(|pos| Info { pos, dir: Dir::N })
             .collect::<Vec<_>>();
         info[0] = Info { pos: 0, dir: Dir::Z };
         Permutations {
-            bound: data.len() - 1,
+            bound: len - 1,
             count: 0,
             data,
             info,
@@ -68,27 +68,30 @@ impl Permutations {
 impl Iterator for Permutations {
     type Item = (usize, usize);
     fn next(&mut self) -> Option<Self::Item> {
-        let (init_idx, &Info { pos: init_pos, dir: init_dir }) = self.info.iter()
+        let (init_idx, init_pos, mut init_dir) = self.info.iter_mut()
             .enumerate()
             .rev()
             .filter(|(_, Info { dir, .. })| *dir != Dir::Z)
+            .map(|(idx, Info { pos, dir })| (idx, *pos, *dir))
             .next()?;
 
         let swap_pos = init_dir.step(init_pos);
-        let swap_idx = self.data[swap_pos];
+        let swap_idx = self.data[swap_pos as usize] as usize;
         let swap_dir = self.info[swap_idx].dir;
 
-        let next_dir = if swap_pos == 0
-        || swap_pos == self.bound
-        || self.data[init_dir.step(swap_pos)] > init_idx {
-            Dir::Z
+        if swap_pos == 0 || swap_pos == self.bound {
+            init_dir = Dir::Z;
         } else {
-            init_dir
+            let next_pos = init_dir.step(swap_pos);
+            let next_idx = self.data[next_pos as usize] as usize;
+            if next_idx > init_idx {
+                init_dir = Dir::Z;
+            }
         };
 
-        self.info[init_idx] = Info { pos: swap_pos, dir: next_dir };
+        self.info[init_idx] = Info { pos: swap_pos, dir: init_dir };
         self.info[swap_idx] = Info { pos: init_pos, dir: swap_dir };
-        self.data.swap(init_pos, swap_pos);
+        self.data.swap(init_pos as usize, swap_pos as usize);
         self.count += 1;
 
         for (_, Info { pos, dir }) in self.info.iter_mut()
@@ -96,11 +99,7 @@ impl Iterator for Permutations {
             .rev()
             .take_while(|(idx, _)| *idx > init_idx)
         {
-            *dir = if *pos < swap_pos {
-                Dir::P
-            } else {
-                Dir::N
-            };
+            *dir = if *pos < swap_pos { Dir::P } else { Dir::N };
         }
 
         Some((init_idx, swap_idx))
@@ -108,7 +107,7 @@ impl Iterator for Permutations {
 
     fn size_hint(&self) -> (usize, Option<usize>) {
         let p = (1..=self.data.len()).product::<usize>();
-        let n = p - self.count - 1;
+        let n = p - self.count as usize - 1;
         (n, Some(n))
     }
 }
