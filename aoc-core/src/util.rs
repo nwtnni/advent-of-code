e17b7477ea68697d37e5dbc7a6e6082172e9ef63
Newton Ni <nwtnni@gmail.com>
2019-12-07 03:38:18 -0500

    Optimize Steinhaus-Johnson-Trotter algorithm
    Implement Shimon Even's auxiliary array properly, allowing
    us to iterate over indices in descending order. This changes
    the time complexity of each step from O(n)--linear scans over
    the entire `data` array to find the maximum--to O(i), where
    the largest number to move is `n - i + 1`.
    
    Since most of the time we're moving the largest number, `n`,
    these iterations should then run in `O(1)` time, checking
    only the last element of the `info` array.

--- aoc-core/src/util.rs
+++ aoc-core/src/util.rs
@@ -25,24 +25,42 @@ impl Dir {
     }
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+struct Info {
+    pos: usize,
+    dir: Dir,
+}
+
 /// Implementation of the Steinhaus-Johnson-Trotter algorithm
 ///
 /// https://en.wikipedia.org/wiki/Steinhaus%E2%80%93Johnson%E2%80%93Trotter_algorithm
 #[derive(Clone, Debug)]
 pub struct Permutations {
-    data: Vec<(Dir, usize)>,
-    done: usize,
+    /// Upper bound on indices
+    bound: usize,
+
+    /// Completed permutations
+    count: usize,
+
+    /// Current permutation as indices
+    data: Vec<usize>,
+
+    /// Position and direction of each index
+    info: Vec<Info>,
 }
 
 impl Permutations {
     fn new(len: usize) -> Self {
-        let mut data = (0..len)
-            .map(|idx| (Dir::N, idx))
+        let data = (0..len).collect::<Vec<_>>();
+        let mut info = (0..len)
+            .map(|pos| Info { pos, dir: Dir::N })
             .collect::<Vec<_>>();
-        data[0] = (Dir::Z, 0);
+        info[0] = Info { pos: 0, dir: Dir::Z };
         Permutations {
+            bound: data.len() - 1,
+            count: 0,
             data,
-            done: 0,
+            info,
         }
     }
 }
@@ -50,34 +68,39 @@ impl Permutations {
 impl Iterator for Permutations {
     type Item = (usize, usize);
     fn next(&mut self) -> Option<Self::Item> {
-        let (init_idx, init_dir, init_ele) = self.data
-            .iter()
+        let (init_idx, &Info { pos: init_pos, dir: init_dir }) = self.info.iter()
             .enumerate()
-            .filter(|(_, (dir, _))| *dir != Dir::Z)
-            .max_by_key(|(_, (_, ele))| ele)
-            .map(|(idx, (dir, ele))| (idx, *dir, *ele))?;
+            .rev()
+            .filter(|(_, Info { dir, .. })| *dir != Dir::Z)
+            .next()?;
 
-        let swap_idx = init_dir.step(init_idx);
-        let swap_dir = if swap_idx == 0
-        || swap_idx == self.data.len() - 1
-        || self.data[init_dir.step(swap_idx)].1 > init_ele {
+        let swap_pos = init_dir.step(init_pos);
+        let swap_idx = self.data[swap_pos];
+        let swap_dir = self.info[swap_idx].dir;
+
+        let next_dir = if swap_pos == 0
+        || swap_pos == self.bound
+        || self.data[init_dir.step(swap_pos)] > init_idx {
             Dir::Z
         } else {
             init_dir
         };
 
-        self.data.swap(init_idx, swap_idx);
-        self.data[swap_idx].0 = swap_dir;
-        self.done += 1;
+        self.info[init_idx] = Info { pos: swap_pos, dir: next_dir };
+        self.info[swap_idx] = Info { pos: init_pos, dir: swap_dir };
+        self.data.swap(init_pos, swap_pos);
+        self.count += 1;
 
-        for (idx, (dir, ele)) in self.data.iter_mut().enumerate() {
-            if *ele > init_ele && *dir == Dir::Z {
-                *dir = if idx < swap_idx {
-                    Dir::P
-                } else {
-                    Dir::N
-                };
-            }
+        for (_, Info { pos, dir }) in self.info.iter_mut()
+            .enumerate()
+            .rev()
+            .take_while(|(idx, _)| *idx > init_idx)
+        {
+            *dir = if *pos < swap_pos {
+                Dir::P
+            } else {
+                Dir::N
+            };
         }
 
         Some((init_idx, swap_idx))
@@ -85,7 +108,7 @@ impl Iterator for Permutations {
 
     fn size_hint(&self) -> (usize, Option<usize>) {
         let p = (1..=self.data.len()).product::<usize>();
-        let n = p - self.done - 1;
+        let n = p - self.count - 1;
         (n, Some(n))
     }
 }
