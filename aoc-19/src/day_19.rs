d6d15675735405e89cdcd6c546a5bd37fab53df4
Newton Ni <nwtnni@gmail.com>
2019-12-22 17:43:27 -0500

    Implement 2019 D19 P2

--- aoc-19/src/day_19.rs
+++ aoc-19/src/day_19.rs
@@ -1,7 +1,3 @@
-use std::collections::HashSet;
-use std::collections::HashMap;
-use std::str;
-
 use aoc::*;
 
 #[derive(Clone, Debug)]
@@ -13,21 +9,56 @@ impl Fro for TractorBeam {
     }
 }
 
+impl TractorBeam {
+    fn get(&mut self, x: i64, y: i64) -> bool {
+        self.0.reset();
+        self.0.input(x);
+        self.0.input(y);
+        self.0.output().unwrap() == 1
+
+    }
+
+    #[allow(unused)]
+    fn plot(&mut self) {
+        for y in 0..50 {
+            for x in 0..50 {
+                if self.get(x, y) {
+                    print!("█");
+                } else {
+                    print!(" ");
+                }
+            }
+            println!("");
+        }
+    }
+}
+
 impl Solution for TractorBeam {
     fn one(mut self) -> i64 {
         let mut sum = 0;
         for y in 0..50 {
             for x in 0..50 {
-                self.0.input(x).unwrap();
-                self.0.input(y).unwrap();
-                sum += self.0.output().unwrap();
-                self.0.reset();
+                sum += self.get(x, y) as i64;
             }
         }
         sum
     }
 
-    fn two(self) -> i64 {
-        todo!()
+    fn two(mut self) -> i64 {
+        let mut x = 3;
+        let mut y = 4;
+
+        loop {
+            if y >= 99 && self.get(x + 99, y - 99) {
+                return x * 10000 + y - 99;
+            }
+            x += 1;
+            while self.get(x, y + 1) {
+                y += 1;
+                if y >= 99 && self.get(x + 99, y - 99) {
+                    return x * 10000 + y - 99;
+                }
+            }
+        }
     }
 }
