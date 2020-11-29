c413760433b51358ba2b12c42bdc9e32dac23193
Newton Ni <nwtnni@gmail.com>
2020-11-28 19:36:14 -0500

    Implement alphabet bitset

--- aoc-core/src/util.rs
+++ aoc-core/src/util.rs
@@ -46,6 +46,72 @@ pub fn permute(len: u8) -> impl Iterator<Item = (usize, usize)> {
     iter::once((0, 0)).chain(Permutations::new(len))
 }
 
+/// Bitset for case-insensitive ASCII letters
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct AlphaSet(u32);
+
+impl AlphaSet {
+    pub fn new() -> Self {
+        AlphaSet::default()
+    }
+
+    pub fn insert(&mut self, alpha: char) -> bool {
+        let mask = Self::mask(alpha);
+        if self.0 & mask > 0 {
+            false
+        } else {
+            self.0 |= mask;
+            true
+        }
+    }
+
+    pub fn contains(&self, alpha: char) -> bool {
+        let mask = Self::mask(alpha);
+        self.0 & mask > 0
+    }
+
+    pub fn and(&self, other: &Self) -> Self {
+        Self(self.0 & other.0)
+    }
+
+    pub fn or(&self, other: &Self) -> Self {
+        Self(self.0 | other.0)
+    }
+
+    pub fn xor(&self, other: &Self) -> Self {
+        Self(self.0 ^ other.0)
+    }
+
+    pub fn not(&self) -> Self {
+        Self(!self.0 & 0b0000_0011_1111_1111_1111_1111_1111_1111)
+    }
+
+    pub fn sub(&self, other: &Self) -> Self {
+        self.and(&other.not())
+    }
+
+    fn mask(alpha: char) -> u32 {
+        assert!(alpha.is_ascii_alphabetic());
+        1 << (((alpha.to_ascii_lowercase() as u32) as u8) - b'a')
+    }
+}
+
+impl<'a> From<&'a str> for AlphaSet {
+    fn from(string: &'a str) -> Self {
+        string.chars().collect()
+    }
+}
+
+impl iter::FromIterator<char> for AlphaSet {
+    fn from_iter<T: IntoIterator<Item = char>>(iter: T) -> Self {
+        let mut set = AlphaSet::new();
+        for char in iter {
+            set.insert(char);
+        }
+        set
+    }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum Dir { P, Z, N, }
 
