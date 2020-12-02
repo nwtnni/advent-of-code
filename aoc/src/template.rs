79e92a723b89e628356c48f8e7d0d62e25f016ad
Newton Ni <nwtnni@gmail.com>
2020-12-02 09:44:57 -0500

    Update template to parse line-based and grid-based input

--- aoc/src/template.rs
+++ aoc/src/template.rs
@@ -3,17 +3,60 @@ use std::collections::HashSet;
 
 use aoc::*;
 
-pub struct $TITLE(String);
+pub struct $TITLE(Vec<...>);
+pub struct $TITLE(HashSet<...>);
+pub struct $TITLE {
+    cols: usize,
+    rows: usize,
+    grid: HashMap<Pos, ...>,
+}
 
+// Line-based parsing
 impl Fro for $TITLE {
     fn fro(input: &str) -> Self {
-        Self(String::from(input))
+        input
+            .trim()
+            .split('\n')
+            .map(|line| {
+
+            })
+            .collect::<Vec<_>>()
+            // .collect::<HashMap<_, _>>()
+            // .collect::<HashSet<_>>()
+            .tap(Self)
+    }
+}
+
+// Grid-based parsing
+impl Fro for $TITLE {
+    fn fro(input: &str) -> Self {
+        let mut grid = HashMap::new();
+        let mut rows = 0;
+        let mut cols = 0;
+        for (y, row) in input.trim().split('\n').enumerate() {
+            rows += 1;
+            cols = 0;
+            for (x, col) in row.trim().chars() {
+                cols += 1;
+                let pos = Pos {
+                    x: x as i64,
+                    y: y as i64,
+                };
+
+                let cell = match col {
+
+                };
+
+                grid.insert(pos, cell);
+            }
+        }
+        Self(grid)
     }
 }
 
 impl Solution for $TITLE {
     fn one(self) -> i64 {
-        todo!()
+
     }
 
     fn two(self) -> i64 {
