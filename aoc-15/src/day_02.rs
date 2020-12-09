40f246964d1b204ccda0f1b69b9220ce8c265a5c
Newton Ni <nwtnni@gmail.com>
2020-12-08 22:44:00 -0500

    Refactor 2015 day 2

--- aoc-15/src/day_02.rs
+++ aoc-15/src/day_02.rs
@@ -1,25 +1,27 @@
 use std::cmp;
-use std::str;
 
+use aoc::*;
+
+#[derive(Clone, Debug)]
 pub struct IWasToldThereWouldBeNoMath(Vec<Present>);
 
-impl str::FromStr for IWasToldThereWouldBeNoMath {
-    type Err = aoc::Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.split_whitespace()
-            .map(Present::from_str)
-            .collect::<Result<Vec<_>, _>>()
-            .map(IWasToldThereWouldBeNoMath)
+impl Fro for IWasToldThereWouldBeNoMath {
+    fn fro(input: &str) -> Self {
+        input
+            .split_whitespace()
+            .map(Present::fro)
+            .collect::<Vec<_>>()
+            .tap(Self)
     }
 }
 
-impl aoc::Solution for IWasToldThereWouldBeNoMath {
+impl Solution for IWasToldThereWouldBeNoMath {
     fn one(self) -> i64 {
-        self.0.iter().map(Present::wrapping).sum()
+        self.0.into_iter().map(Present::wrapping).sum()
     }
 
     fn two(self) -> i64 {
-        self.0.iter().map(Present::ribbon).sum()
+        self.0.into_iter().map(Present::ribbon).sum()
     }
 }
 
@@ -30,39 +32,36 @@ pub struct Present {
     h: i64,
 }
 
-impl str::FromStr for Present {
-    type Err = aoc::Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut iter = s.split('x')
-            .map(i64::from_str)
-            .filter_map(Result::ok);
-        let l = iter.next().unwrap();
-        let w = iter.next().unwrap();
-        let h = iter.next().unwrap();
-        Ok(Present { l, w, h })
+impl Fro for Present {
+    fn fro(input: &str) -> Self {
+        let mut iter = input.split('x').map(i64::fro);
+        let l = iter.give();
+        let w = iter.give();
+        let h = iter.give();
+        Present { l, w, h }
     }
 }
 
 impl Present {
-    pub fn area(&self) -> i64 {
+    pub fn area(self) -> i64 {
         2 * self.l * self.w +
         2 * self.w * self.h +
         2 * self.h * self.l
     }
 
-    pub fn volume(&self) -> i64 {
+    pub fn volume(self) -> i64 {
         self.l * self.w * self.h
     }
 
-    pub fn slack(&self) -> i64 {
+    pub fn slack(self) -> i64 {
         cmp::min(self.l * self.w, cmp::min(self.w * self.h, self.h * self.l))
     }
 
-    pub fn wrapping(&self) -> i64 {
+    pub fn wrapping(self) -> i64 {
         self.area() + self.slack()
     }
 
-    pub fn ribbon(&self) -> i64 {
+    pub fn ribbon(self) -> i64 {
         let lw = 2 * (self.l + self.w);
         let wh = 2 * (self.w + self.h);
         let hl = 2 * (self.h + self.l);
@@ -77,22 +76,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_1_0() {
+    fn part_one_0() {
         assert_eq!(Present { l: 2, w: 3, h: 4 }.wrapping(), 58)
     }
 
     #[test]
-    fn test_1_1() {
+    fn part_one_1() {
         assert_eq!(Present { l: 1, w: 1, h: 10 }.wrapping(), 43)
     }
 
     #[test]
-    fn test_2_0() {
+    fn part_two_0() {
         assert_eq!(Present { l: 2, w: 3, h: 4 }.ribbon(), 34)
     }
 
     #[test]
-    fn test_2_1() {
+    fn part_two_1() {
         assert_eq!(Present { l: 1, w: 1, h: 10 }.ribbon(), 14)
     }
 }
