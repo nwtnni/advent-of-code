392e66fbe797578c2c32e1aa834d634b72d9a7aa
Newton Ni <nwtnni@gmail.com>
2020-12-08 22:43:30 -0500

    Refactor 2015 day 1

--- aoc-15/src/day_01.rs
+++ aoc-15/src/day_01.rs
@@ -1,63 +1,69 @@
-use std::str;
+use std::iter;
+use std::mem;
 
-pub struct NotQuiteLisp(String);
+use aoc::*;
 
-impl str::FromStr for NotQuiteLisp {
-    type Err = aoc::Error;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(NotQuiteLisp(s.to_owned()))
+#[derive(Clone, Debug)]
+pub struct NotQuiteLisp(Vec<i64>);
+
+impl Fro for NotQuiteLisp {
+    fn fro(input: &str) -> Self {
+        input
+            .trim()
+            .chars()
+            .map(|char| {
+                match char {
+                | '(' => 1,
+                | ')' => -1,
+                | _ => unreachable!(),
+                }
+            })
+            .collect::<Vec<_>>()
+            .tap(Self)
     }
 }
 
-fn delta(c: char) -> i64 {
-    match c {
-    | '(' =>  1,
-    | ')' => -1,
-    | _   =>  0,
-    }
-}
-
-impl aoc::Solution for NotQuiteLisp {
+impl Solution for NotQuiteLisp {
     fn one(self) -> i64 {
-        self.0.chars()
-            .map(delta)
-            .sum()
+        self.0.into_iter().sum()
     }
 
     fn two(self) -> i64 {
-        self.0.chars()
-            .map(delta)
-            .scan(0, |this, next| {
-                if *this == -1 { None } else { Some(*this += next) }
-            })
-            .count() as i64
+        self.0
+            .into_iter()
+            .chain(iter::once(0))
+            .scan(0, |floor, next| Some(mem::replace(floor, *floor + next)))
+            .position(|floor| floor == -1)
+            .unwrap()
+            as i64
     }
 }
 
 #[cfg(test)]
 mod tests {
 
-    use aoc::Solution;
+    use aoc::Fro as _;
+    use aoc::Solution as _;
 
     macro_rules! impl_test {
         ($name:ident, $method:ident, $input:expr, $value:expr) => {
             #[test]
             fn $name() {
-                assert_eq!(super::NotQuiteLisp(String::from($input)).$method(), $value)
+                assert_eq!(super::NotQuiteLisp::fro($input).$method(), $value)
             }
         }
     }
 
-    impl_test!(test_1_0, one, "(())", 0);
-    impl_test!(test_1_1, one, "()()", 0);
-    impl_test!(test_1_2, one, "(((", 3);
-    impl_test!(test_1_3, one, "(()(()(", 3);
-    impl_test!(test_1_4, one, "))(((((", 3);
-    impl_test!(test_1_5, one, "())", -1);
-    impl_test!(test_1_6, one, "))(", -1);
-    impl_test!(test_1_7, one, ")))", -3);
-    impl_test!(test_1_8, one, ")())())", -3);
+    impl_test!(part_one_0, one, "(())", 0);
+    impl_test!(part_one_1, one, "()()", 0);
+    impl_test!(part_one_2, one, "(((", 3);
+    impl_test!(part_one_3, one, "(()(()(", 3);
+    impl_test!(part_one_4, one, "))(((((", 3);
+    impl_test!(part_one_5, one, "())", -1);
+    impl_test!(part_one_6, one, "))(", -1);
+    impl_test!(part_one_7, one, ")))", -3);
+    impl_test!(part_one_8, one, ")())())", -3);
 
-    impl_test!(test_2_0, two, ")", 1);
-    impl_test!(test_2_1, two, "()())", 5);
+    impl_test!(part_two_0, two, ")", 1);
+    impl_test!(part_two_1, two, "()())", 5);
 }
