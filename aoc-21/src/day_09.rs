a59ea25f4c4ff440a6d4635835f3073337dff1f1
Newton Ni <nwtnni@gmail.com>
2021-12-09 01:05:41 -0500

    Clean up 2021 day 9

--- aoc-21/src/day_09.rs
+++ aoc-21/src/day_09.rs
@@ -18,126 +18,82 @@ impl Fro for SmokeBasin {
 
 impl Solution for SmokeBasin {
     fn one(self) -> i64 {
-        let h = self.0.len();
-        let w = self.0[0].len();
-
-        let mut risk = 0;
-
-        for i in 0..h {
-            for j in 0..w {
-                let height = self.0[i][j];
-
-                if let Some(&up) = self.0.get(i.wrapping_sub(1)).and_then(|row| row.get(j)) {
-                    if up <= height {
-                        continue;
-                    }
-                }
-
-                if let Some(&down) = self.0.get(i + 1).and_then(|row| row.get(j)) {
-                    if down <= height {
-                        continue;
-                    }
-                }
-
-                if let Some(&left) = self.0.get(i).and_then(|row| row.get(j.wrapping_sub(1))) {
-                    if left <= height {
-                        continue;
-                    }
-                }
-
-                if let Some(&right) = self.0.get(i).and_then(|row| row.get(j + 1)) {
-                    if right <= height {
-                        continue;
-                    }
-                }
-
-                risk += height + 1;
-            }
-        }
-
-        risk
+        minima(&self.0)
+            .into_iter()
+            .map(|(i, j)| self.0[i][j] + 1)
+            .sum()
     }
 
     fn two(mut self) -> i64 {
-        let h = self.0.len();
-        let w = self.0[0].len();
-
-        let mut minima = Vec::new();
-
-        for i in 0..h {
-            for j in 0..w {
-                let height = self.0[i][j];
-
-                if let Some(&up) = self.0.get(i.wrapping_sub(1)).and_then(|row| row.get(j)) {
-                    if up <= height {
-                        continue;
-                    }
-                }
-
-                if let Some(&down) = self.0.get(i + 1).and_then(|row| row.get(j)) {
-                    if down <= height {
-                        continue;
-                    }
-                }
-
-                if let Some(&left) = self.0.get(i).and_then(|row| row.get(j.wrapping_sub(1))) {
-                    if left <= height {
-                        continue;
-                    }
-                }
-
-                if let Some(&right) = self.0.get(i).and_then(|row| row.get(j + 1)) {
-                    if right <= height {
-                        continue;
-                    }
-                }
-
-                minima.push((i, j));
-            }
-        }
-
-        fn flood((i, j): (usize, usize), count: &mut i64, grid: &mut Vec<Vec<i64>>) {
-            if grid[i][j] >= 9 {
-                return;
-            }
-
-            grid[i][j] = 9;
-            *count += 1;
-
-            if let Some(up) = grid.get_mut(i.wrapping_sub(1)).and_then(|row| row.get_mut(j)) {
-                if *up <= 9 {
-                    flood((i.wrapping_sub(1), j), count, grid);
-                }
-            }
-
-            if let Some(down) = grid.get_mut(i + 1).and_then(|row| row.get_mut(j)) {
-                if *down <= 9 {
-                    flood((i + 1, j), count, grid);
-                }
-            }
-
-            if let Some(left) = grid.get_mut(i).and_then(|row| row.get_mut(j.wrapping_sub(1))) {
-                if *left <= 9 {
-                    flood((i, j.wrapping_sub(1)), count, grid);
-                }
-            }
-
-            if let Some(right) = grid.get_mut(i).and_then(|row| row.get_mut(j + 1)) {
-                if *right <= 9 {
-                    flood((i, j + 1), count, grid);
-                }
-            }
-        }
-
+        let minima = minima(&self.0);
         let mut basins = Vec::new();
 
         for point in minima {
-            let mut count = 0;
-            flood(point, &mut count, &mut self.0);
-            basins.push(count);
+            let mut size = 0;
+            flood(point, &mut size, &mut self.0);
+            basins.push(size);
         }
 
-        basins.sort();
-        basins.into_iter().rev().take(3).product::<i64>()
+        basins
+            .tap_mut(|basins| basins.sort())
+            .into_iter()
+            .rev()
+            .take(3)
+            .product()
     }
+}
+
+fn minima(grid: &[Vec<i64>]) -> Vec<(usize, usize)> {
+    let h = grid.len();
+    let w = grid[0].len();
+    let mut minima = Vec::new();
+
+    for i in 0..h {
+        for j in 0..w {
+            let height = grid[i][j];
+
+            if let Some(&up) = grid.get(i.wrapping_sub(1)).and_then(|row| row.get(j)) {
+                if up <= height {
+                    continue;
+                }
+            }
+
+            if let Some(&down) = grid.get(i + 1).and_then(|row| row.get(j)) {
+                if down <= height {
+                    continue;
+                }
+            }
+
+            if let Some(&left) = grid.get(i).and_then(|row| row.get(j.wrapping_sub(1))) {
+                if left <= height {
+                    continue;
+                }
+            }
+
+            if let Some(&right) = grid.get(i).and_then(|row| row.get(j + 1)) {
+                if right <= height {
+                    continue;
+                }
+            }
+
+            minima.push((i, j));
+        }
+    }
+
+    minima
+}
+
+fn flood((i, j): (usize, usize), size: &mut i64, grid: &mut Vec<Vec<i64>>) {
+    match grid.get_mut(i).and_then(|row| row.get_mut(j)) {
+        None | Some(9) => return,
+        Some(height) => {
+            *height = 9;
+            *size += 1;
+        }
+    }
+
+    flood((i.wrapping_sub(1), j), size, grid);
+    flood((i + 1, j), size, grid);
+    flood((i, j.wrapping_sub(1)), size, grid);
+    flood((i, j + 1), size, grid);
 }
