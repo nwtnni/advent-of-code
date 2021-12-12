877cac8236167012a28b64273ae8c4e8d7fd3dd7
Newton Ni <nwtnni@gmail.com>
2021-12-12 02:13:25 -0500

    Clean up 2021 day 12

--- aoc-21/src/day_12.rs
+++ aoc-21/src/day_12.rs
@@ -1,100 +1,100 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 use aoc::*;
-use petgraph::graphmap::UnGraphMap;
 
 #[derive(Clone, Debug)]
-pub struct PassagePathing(UnGraphMap<&'static str, ()>);
+pub struct PassagePathing(HashMap<&'static str, Vec<&'static str>>);
 
 impl Fro for PassagePathing {
     fn fro(input: &str) -> Self {
+        let mut graph = HashMap::<_, Vec<_>>::new();
+
         input
             .trim()
             .split('\n')
-            .map(|line| {
+            .flat_map(|line| {
                 let (a, b) = line.split_once('-').unwrap();
-                (a.leak(), b.leak(), ())
+                let (a, b) = (a.leak(), b.leak());
+                [(a, b), (b, a)]
             })
-            .collect::<UnGraphMap<_, _>>()
-            .tap(Self)
+            .for_each(|(a, b)| {
+                graph.entry(a).or_default().push(b);
+            });
+
+        Self(graph)
     }
 }
 
 impl Solution for PassagePathing {
-    fn one(mut self) -> i64 {
-        let mut total = 0;
-        recurse(&mut self.0, "start", &mut total);
-        total as i64
-    }
+    fn one(self) -> i64 {
+        fn recurse(
+            graph: &HashMap<&'static str, Vec<&'static str>>,
+            here: &'static str,
+            visited: &mut HashSet<&'static str>,
+        ) -> i64 {
+            let mut count = 0;
+            visited.insert(here);
 
-    fn two(mut self) -> i64 {
-        let mut all = HashSet::new();
-        recurse2(&mut self.0, "start", false, &mut vec![], &mut all);
-        all.len() as i64
-    }
-}
+            for next in graph.get(here).into_iter().flatten() {
+                if *next == "end" {
+                    count += 1;
+                    continue;
+                }
 
-fn recurse(graph: &mut UnGraphMap<&'static str, ()>, here: &'static str, total: &mut usize) {
-    if here == "end" {
-        *total += 1;
-        return;
-    }
+                if next.chars().all(|char| char.is_ascii_lowercase()) && visited.contains(next) {
+                    continue;
+                }
 
-    if !petgraph::algo::dijkstra(&*graph, here, Some("end"), |_| 1).contains_key("end") {
-        return;
-    }
-
-    let neighbors = graph.neighbors(here).collect::<Vec<_>>();
-
-    if here.chars().all(|char| char.is_ascii_lowercase()) {
-        graph.remove_node(here);
-    }
-
-    for next in &neighbors {
-        recurse(graph, next, total);
-    }
-
-    if here.chars().all(|char| char.is_ascii_lowercase()) {
-        graph.extend(neighbors.into_iter().map(|neighbor| (here, neighbor, ())));
-    }
-}
-
-fn recurse2(graph: &mut UnGraphMap<&'static str, ()>, here: &'static str, twice: bool, path: &mut Vec<&'static str>, all: &mut HashSet<Vec<&'static str>>) {
-    path.push(here);
-
-    if here == "end" {
-        all.insert(path.clone());
-        path.pop();
-        return;
-    }
-
-    if !petgraph::algo::dijkstra(&*graph, here, Some("end"), |_| 1).contains_key("end") {
-        path.pop();
-        return;
-    }
-
-    if here.chars().all(|char| char.is_ascii_lowercase()) {
-        if !twice && here != "start" && here != "end" {
-            for next in graph.neighbors(here).collect::<Vec<_>>() {
-                recurse2(graph, next, true, path, all);
+                count += recurse(graph, next, visited);
             }
+
+            visited.remove(here);
+            count
         }
 
-        let neighbors = graph.neighbors(here).collect::<Vec<_>>();
-        graph.remove_node(here);
-
-        for next in &neighbors {
-            recurse2(graph, next, twice, path, all);
-        }
-
-        graph.extend(neighbors.into_iter().map(|neighbor| (here, neighbor, ())));
+        recurse(&self.0, "start", &mut HashSet::new())
     }
 
-    if here.chars().all(|char| char.is_ascii_uppercase()) {
-        for next in graph.neighbors(here).collect::<Vec<_>>() {
-            recurse2(graph, next, twice, path, all);
-        }
-    }
+    fn two(self) -> i64 {
+        fn recurse(
+            graph: &HashMap<&'static str, Vec<&'static str>>,
+            here: &'static str,
+            visited: &mut HashSet<&'static str>,
+            twice: bool,
+        ) -> i64 {
+            let mut count = 0;
 
-    path.pop();
+            for next in graph
+                .get(here)
+                .into_iter()
+                .flatten()
+                .filter(|next| **next != "start")
+            {
+                if *next == "end" {
+                    count += 1;
+                    continue;
+                }
+
+                if next.chars().all(|char| char.is_ascii_uppercase()) {
+                    count += recurse(graph, next, visited, twice);
+                    continue;
+                }
+
+                match (visited.contains(next), twice) {
+                    (true, true) => (),
+                    (true, false) => count += recurse(graph, next, visited, true),
+                    (false, _) => {
+                        visited.insert(next);
+                        count += recurse(graph, next, visited, twice);
+                        visited.remove(next);
+                    }
+                }
+            }
+
+            count
+        }
+
+        recurse(&self.0, "start", &mut HashSet::new(), false)
+    }
 }
