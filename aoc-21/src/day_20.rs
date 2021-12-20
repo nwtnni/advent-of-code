8db35fbef6b6f5dde3dae24d2f7ee7256da5b25d
Newton Ni <nwtnni@gmail.com>
2021-12-20 01:10:36 -0500

    Clean up 2021 day 20

--- aoc-21/src/day_20.rs
+++ aoc-21/src/day_20.rs
@@ -1,12 +1,10 @@
+use std::cmp;
 use std::collections::HashMap;
-use std::collections::HashSet;
 
 use aoc::*;
 
 #[derive(Clone, Debug)]
 pub struct TrenchMap {
-    cols: usize,
-    rows: usize,
     grid: HashMap<Pos, bool>,
     enhance: Vec<bool>,
 }
@@ -22,123 +20,80 @@ impl Fro for TrenchMap {
             .map(|char| char == '#')
             .collect::<Vec<_>>();
 
-        assert_eq!(enhance.len(), 512);
-
-        let mut lines = iter.give();
-
+        let lines = iter.give();
         let mut grid = HashMap::new();
-        let mut rows = 0;
-        let mut cols = 0;
+
         for (y, row) in lines.trim().split('\n').enumerate() {
-            rows += 1;
-            cols = 0;
             for (x, col) in row.trim().chars().enumerate() {
-                cols += 1;
                 let pos = Pos {
                     x: x as i64,
                     y: y as i64,
                 };
 
-                let cell = match col {
-                    '#' => true,
-                    '.' => false,
-                    _ => unreachable!(),
-                };
-
-                grid.insert(pos, cell);
+                grid.insert(pos, col == '#');
             }
         }
-        Self {
-            cols,
-            rows,
-            grid,
-            enhance,
-        }
+
+        Self { grid, enhance }
     }
 }
 
 impl Solution for TrenchMap {
     fn one(mut self) -> i64 {
-        let mut next = HashMap::new();
-
-        for round in 0..2 {
-            let min_x = self.grid.keys().map(|pos| pos.x).min().unwrap();
-            let min_y = self.grid.keys().map(|pos| pos.y).min().unwrap();
-
-            let max_x = self.grid.keys().map(|pos| pos.x).max().unwrap();
-            let max_y = self.grid.keys().map(|pos| pos.y).max().unwrap();
-
-            next.clear();
-
-            for i in min_y - 3..=max_y + 3 {
-                for j in min_x - 3..=max_x + 3 {
-                    let mut index = 0;
-                    let mut bit = 8;
-
-                    for di in -1..=1 {
-                        for dj in -1..=1 {
-                            index |= (*self
-                                .grid
-                                .get(&Pos {
-                                    x: j + dj,
-                                    y: i + di,
-                                })
-                                .unwrap_or(&if round % 2 == 0 { false } else { true })
-                                as usize)
-                                << bit;
-                            bit -= 1;
-                        }
-                    }
-
-                    next.insert(Pos { x: j, y: i }, self.enhance[index]);
-                }
-            }
-
-            std::mem::swap(&mut self.grid, &mut next);
-        }
-
-        self.grid.values().filter(|x| **x).count() as i64
+        self.enhance(2)
     }
 
     fn two(mut self) -> i64 {
-        let mut next = HashMap::new();
+        self.enhance(50)
+    }
+}
 
-        for round in 0..50 {
-            let min_x = self.grid.keys().map(|pos| pos.x).min().unwrap();
-            let min_y = self.grid.keys().map(|pos| pos.y).min().unwrap();
+impl TrenchMap {
+    fn enhance(&mut self, rounds: usize) -> i64 {
+        let mut buffer = HashMap::new();
 
-            let max_x = self.grid.keys().map(|pos| pos.x).max().unwrap();
-            let max_y = self.grid.keys().map(|pos| pos.y).max().unwrap();
+        for round in 0..rounds {
+            buffer.clear();
 
-            next.clear();
+            let (min_i, max_i, min_j, max_j) = self.grid.keys().fold(
+                (i64::MAX, i64::MIN, i64::MAX, i64::MIN),
+                |(min_i, max_i, min_j, max_j), Pos { x, y }| {
+                    (
+                        cmp::min(min_i, *y),
+                        cmp::max(max_i, *y),
+                        cmp::min(min_j, *x),
+                        cmp::max(max_j, *x),
+                    )
+                },
+            );
 
-            for i in min_y - 3..=max_y + 3 {
-                for j in min_x - 3..=max_x + 3 {
+            for i in min_i - 2..=max_i + 2 {
+                for j in min_j - 2..=max_j + 2 {
                     let mut index = 0;
                     let mut bit = 8;
 
                     for di in -1..=1 {
                         for dj in -1..=1 {
-                            index |= (*self
+                            let on = self
                                 .grid
                                 .get(&Pos {
                                     x: j + dj,
                                     y: i + di,
                                 })
-                                .unwrap_or(&if round % 2 == 0 { false } else { true })
-                                as usize)
-                                << bit;
+                                .copied()
+                                .unwrap_or(round % 2 == 1);
+                            index |= (on as usize) << bit;
                             bit -= 1;
                         }
                     }
 
-                    next.insert(Pos { x: j, y: i }, self.enhance[index]);
+                    buffer.insert(Pos { x: j, y: i }, self.enhance[index]);
                 }
             }
 
-            std::mem::swap(&mut self.grid, &mut next);
+            std::mem::swap(&mut self.grid, &mut buffer);
         }
 
-        self.grid.values().filter(|x| **x).count() as i64
+        self.grid.values().filter(|on| **on).count() as i64
     }
 }
