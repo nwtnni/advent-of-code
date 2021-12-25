755f586d77984c50c431862f7e955c5231076832
Newton Ni <nwtnni@gmail.com>
2021-12-25 03:39:23 -0500

    Port 2018 day 3

---
+++ aoc-18/src/day_03.rs
@@ -0,0 +1,92 @@
+use std::collections::HashMap;
+
+use aoc::*;
+
+#[derive(Clone, Debug)]
+pub struct NoMatterHowYouSliceIt(Vec<Fabric>);
+
+#[derive(Copy, Clone, Debug)]
+struct Fabric {
+    x: i64,
+    y: i64,
+    w: i64,
+    h: i64,
+}
+
+impl Fro for NoMatterHowYouSliceIt {
+    fn fro(input: &str) -> Self {
+        input
+            .trim()
+            .split('\n')
+            .map(Fabric::fro)
+            .collect::<Vec<_>>()
+            .tap(Self)
+    }
+}
+
+impl Fro for Fabric {
+    fn fro(input: &str) -> Self {
+        let (_, claim) = input.trim().split_once(" @ ").unwrap();
+
+        let (pos, dim) = claim.split_once(": ").unwrap();
+
+        let (x, y) = pos.split_once(',').unwrap();
+        let (w, h) = dim.split_once('x').unwrap();
+
+        Self {
+            x: i64::fro(x),
+            y: i64::fro(y),
+            w: i64::fro(w),
+            h: i64::fro(h),
+        }
+    }
+}
+
+impl Solution for NoMatterHowYouSliceIt {
+    fn one(self) -> i64 {
+        let mut claimed = HashMap::new();
+
+        for fabric in &self.0 {
+            for y in fabric.y..fabric.y + fabric.h {
+                for x in fabric.x..fabric.x + fabric.w {
+                    match claimed.get_mut(&(x, y)) {
+                        None => claimed.insert((x, y), false).tap(drop),
+                        Some(twice) => *twice = true,
+                    }
+                }
+            }
+        }
+
+        claimed.values().filter(|twice| **twice).count() as i64
+    }
+
+    fn two(self) -> i64 {
+        let mut claimed = HashMap::new();
+        let mut clean = vec![true; self.0.len()];
+
+        for (claim, fabric) in self.0.iter().enumerate() {
+            for y in fabric.y..fabric.y + fabric.h {
+                for x in fabric.x..fabric.x + fabric.w {
+                    match claimed.get_mut(&(x, y)) {
+                        None => {
+                            claimed.insert((x, y), Some(claim));
+                        }
+                        Some(previous) => {
+                            if let Some(id) = previous {
+                                clean[*id] = false;
+                            }
+                            clean[claim] = false;
+                            *previous = None;
+                        }
+                    }
+                }
+            }
+        }
+
+        clean
+            .into_iter()
+            .position(std::convert::identity)
+            .map(|claim| claim as i64 + 1)
+            .unwrap()
+    }
+}
