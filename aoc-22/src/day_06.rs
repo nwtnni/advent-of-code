08e2c5500c2a20a55f65f4149781205d37afa7d9
Newton Ni <nwtnni@gmail.com>
2022-12-05 23:10:57 -0600

    Refactor 2022 day 6

--- aoc-22/src/day_06.rs
+++ aoc-22/src/day_06.rs
@@ -3,36 +3,37 @@ use std::collections::HashSet;
 use aoc::*;
 
 #[derive(Clone, Debug)]
-pub struct TuningTrouble(Vec<char>);
+pub struct TuningTrouble(String);
 
 impl Fro for TuningTrouble {
     fn fro(input: &str) -> Self {
-        Self(input.chars().collect())
+        Self(String::from(input.trim()))
     }
 }
 
 impl Solution for TuningTrouble {
     fn one(self) -> i64 {
-        for (i, a) in self.0.windows(4).enumerate() {
-            if a[0] != a[1]
-                && a[1] != a[2]
-                && a[2] != a[3]
-                && a[0] != a[2]
-                && a[0] != a[3]
-                && a[1] != a[3]
-            {
-                return 4 + i as i64;
-            }
-        }
-        panic!()
+        self.start(4) as i64
     }
 
     fn two(self) -> i64 {
-        for (i, a) in self.0.windows(14).enumerate() {
-            if a.iter().collect::<HashSet<_>>().len() == 14 {
-                return 14 + i as i64;
-            }
-        }
-        panic!()
+        self.start(14) as i64
+    }
+}
+
+impl TuningTrouble {
+    fn start(&self, length: usize) -> usize {
+        let mut set = HashSet::new();
+
+        self.0
+            .as_bytes()
+            .windows(length)
+            .position(|message| {
+                set.clear();
+                set.extend(message.iter().copied());
+                set.len() == length
+            })
+            .unwrap()
+            + length
     }
 }
