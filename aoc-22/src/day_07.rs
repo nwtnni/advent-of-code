cf9925db2cfc91a4dfe37d30643117485e1813b9
Newton Ni <nwtnni@gmail.com>
2022-12-06 23:49:18 -0600

    Refactor 2022 day 7

--- aoc-22/src/day_07.rs
+++ aoc-22/src/day_07.rs
@@ -15,43 +15,42 @@ enum Command {
 
 #[derive(Clone, Debug)]
 enum Entry {
-    Dir(String),
+    Directory(String),
     File(i64, String),
 }
 
 impl Fro for NoSpaceLeftOnDevice {
     fn fro(input: &str) -> Self {
         let mut iter = input.trim().lines().peekable();
-
         let mut commands = Vec::new();
+
         while let Some(next) = iter.next() {
-            if next.starts_with("$ ") {
-                let next = next.trim_start_matches("$ ");
+            let command = next.strip_prefix("$ ").unwrap();
 
-                if next.starts_with("cd ") {
-                    let directory = next.trim_start_matches("cd ").trim().to_owned();
-                    commands.push(Command::Cd(directory));
-                } else if next.starts_with("ls") {
-                    let mut ls = Vec::new();
-
-                    while let Some(a) = iter.peek() {
-                        if a.starts_with("$ ") {
-                            break;
-                        }
-
-                        if a.starts_with("dir ") {
-                            ls.push(Entry::Dir(a.trim_start_matches("dir ").trim().to_owned()));
-                        } else {
-                            let (a, b) = a.split_once(' ').unwrap();
-                            ls.push(Entry::File(i64::fro(a), b.to_owned()));
-                        }
-
-                        iter.next();
-                    }
-
-                    commands.push(Command::Ls(ls));
-                }
+            if let Some(directory) = command.strip_prefix("cd ") {
+                commands.push(Command::Cd(String::from(directory)));
+                continue;
             }
+
+            assert_eq!(command, "ls");
+            let mut entries = Vec::new();
+
+            while let Some(entry) = iter.peek() {
+                if entry.starts_with("$ ") {
+                    break;
+                }
+
+                if let Some(directory) = entry.strip_prefix("dir ") {
+                    entries.push(Entry::Directory(String::from(directory)));
+                } else {
+                    let (size, file) = entry.split_once(' ').unwrap();
+                    entries.push(Entry::File(i64::fro(size), String::from(file)));
+                }
+
+                iter.next();
+            }
+
+            commands.push(Command::Ls(entries));
         }
 
         Self(commands)
@@ -60,54 +59,15 @@ impl Fro for NoSpaceLeftOnDevice {
 
 impl Solution for NoSpaceLeftOnDevice {
     fn one(self) -> i64 {
-        let mut prefix = Path::new("/").to_path_buf();
-        let mut tree = HashMap::new();
-        let mut sizes = HashMap::new();
-
-        for command in self.0 {
-            match &command {
-                Command::Cd(path) if path == ".." => {
-                    prefix.pop();
-                }
-                Command::Cd(path) if path == "/" => prefix = PathBuf::from("/"),
-                Command::Cd(path) => prefix.push(path),
-                Command::Ls(entries) => {
-                    tree.entry(prefix.clone())
-                        .or_insert_with(Vec::new)
-                        .extend(entries.iter().cloned());
-                }
-            }
-        }
-
-        recurse(&tree, &mut Path::new("/").to_path_buf(), &mut sizes);
-        sizes
-            .iter()
-            .filter_map(|(_, size)| if *size <= 100000 { Some(*size) } else { None })
+        self.flatten()
+            .values()
+            .copied()
+            .filter(|size| *size <= 100000)
             .sum()
     }
 
     fn two(self) -> i64 {
-        let mut prefix = Path::new("/").to_path_buf();
-        let mut tree = HashMap::new();
-        let mut sizes = HashMap::new();
-
-        for command in self.0 {
-            match &command {
-                Command::Cd(path) if path == ".." => {
-                    prefix.pop();
-                }
-                Command::Cd(path) if path == "/" => prefix = PathBuf::from("/"),
-                Command::Cd(path) => prefix.push(path),
-                Command::Ls(entries) => {
-                    tree.entry(prefix.clone())
-                        .or_insert_with(Vec::new)
-                        .extend(entries.iter().cloned());
-                }
-            }
-        }
-
-        recurse(&tree, &mut Path::new("/").to_path_buf(), &mut sizes);
-
+        let sizes = self.flatten();
         let total = sizes[Path::new("/")];
 
         sizes
@@ -119,26 +79,49 @@ impl Solution for NoSpaceLeftOnDevice {
     }
 }
 
-fn recurse(
-    tree: &HashMap<PathBuf, Vec<Entry>>,
-    root: &mut PathBuf,
-    sizes: &mut HashMap<PathBuf, i64>,
-) -> i64 {
-    let sum = tree
-        .get(root)
-        .unwrap()
-        .iter()
-        .map(|entry| match entry {
-            Entry::File(size, _) => *size,
-            Entry::Dir(dir) => {
-                root.push(dir);
-                let size = recurse(tree, root, sizes);
-                root.pop();
-                size
-            }
-        })
-        .sum();
+impl NoSpaceLeftOnDevice {
+    fn flatten(self) -> HashMap<PathBuf, i64> {
+        let mut root = Path::new("/").to_path_buf();
+        let mut tree = HashMap::new();
 
-    sizes.insert(root.clone(), sum);
-    sum
+        for command in self.0 {
+            match command {
+                Command::Cd(path) if path == ".." => {
+                    root.pop();
+                }
+                Command::Cd(path) => root.push(path),
+                Command::Ls(entries) => {
+                    tree.insert(root.clone(), entries);
+                }
+            }
+        }
+
+        let mut sizes = HashMap::new();
+        root.push("/");
+        Self::recurse(&mut root, &tree, &mut sizes);
+        sizes
+    }
+
+    // Recursively fill out directory sizes
+    fn recurse(
+        root: &mut PathBuf,
+        tree: &HashMap<PathBuf, Vec<Entry>>,
+        sizes: &mut HashMap<PathBuf, i64>,
+    ) -> i64 {
+        let size = tree[root]
+            .iter()
+            .map(|entry| match entry {
+                Entry::File(size, _) => *size,
+                Entry::Directory(dir) => {
+                    root.push(dir);
+                    let size = Self::recurse(root, tree, sizes);
+                    root.pop();
+                    size
+                }
+            })
+            .sum();
+
+        sizes.insert(root.clone(), size);
+        size
+    }
 }
