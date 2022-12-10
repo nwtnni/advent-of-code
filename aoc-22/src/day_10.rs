c268d4b2ad1a2d1766c96dff416a7e7c4dbe56c6
Newton Ni <nwtnni@gmail.com>
2022-12-09 23:53:09 -0600

    Refactor 2022 day 10

--- aoc-22/src/day_10.rs
+++ aoc-22/src/day_10.rs
@@ -1,12 +1,12 @@
 use aoc::*;
 
 #[derive(Clone, Debug)]
-pub struct CathodeRayTube(Vec<Ins>);
+pub struct CathodeRayTube(Vec<Instruction>);
 
 #[derive(Copy, Clone, Debug)]
-enum Ins {
+enum Instruction {
     Noop,
-    Add(i64),
+    Addx(i64),
 }
 
 impl Fro for CathodeRayTube {
@@ -15,8 +15,8 @@ impl Fro for CathodeRayTube {
             .trim()
             .split('\n')
             .map(|line| match line {
-                "noop" => Ins::Noop,
-                _ => Ins::Add(i64::fro(line.trim_start_matches("addx "))),
+                "noop" => Instruction::Noop,
+                _ => Instruction::Addx(i64::fro(line.trim_start_matches("addx "))),
             })
             .collect::<Vec<_>>()
             .tap(Self)
@@ -25,79 +25,65 @@ impl Fro for CathodeRayTube {
 
 impl Solution for CathodeRayTube {
     fn one(self) -> i64 {
-        let mut cycle = 1;
-        let mut x = 1;
-        let mut pause = false;
         let mut sum = 0;
 
-        let mut iter = self.0.into_iter().peekable();
-        while let Some(ins) = iter.peek() {
-            if cycle > 220 {
-                break;
-            }
-
-            if cycle == 20 || cycle > 20 && (cycle - 20) % 40 == 0 {
+        self.run(|cycle, x| {
+            if cycle <= 220 && (cycle == 20 || cycle > 20 && (cycle - 20) % 40 == 0) {
                 sum += x * cycle;
             }
+        });
 
-            match ins {
-                Ins::Noop => {
-                    iter.next();
-                    cycle += 1;
-                }
-                Ins::Add(dx) if pause => {
-                    pause = false;
-                    x += dx;
-                    cycle += 1;
-                    iter.next();
-                }
-                Ins::Add(_) => {
-                    pause = true;
-                    cycle += 1;
-                }
-            }
-        }
         sum
     }
 
     fn two(self) -> i64 {
-        let mut cycle = 1;
-        let mut x = 1i64;
-        let mut pause = false;
-
-        let mut iter = self.0.into_iter().peekable();
         let mut screen = vec![false; 40 * 6];
 
-        while let Some(ins) = iter.peek() {
+        self.run(|cycle, x| {
             let sx = (cycle - 1) % 40;
             if (x - sx).abs() <= 1 {
                 screen[cycle as usize - 1] = true;
             }
+        });
 
-            match ins {
-                Ins::Noop => {
-                    iter.next();
-                    cycle += 1;
-                }
-                Ins::Add(dx) if pause => {
-                    pause = false;
-                    x += *dx;
-                    cycle += 1;
-                    iter.next();
-                }
-                Ins::Add(_) => {
-                    pause = true;
-                    cycle += 1;
-                }
-            }
-        }
         for i in 0..6 {
             for j in 0..40 {
-                print!("{}", if screen[i * 40 + j] { '#' } else { '.' })
+                print!("{}", if screen[i * 40 + j] { '█' } else { ' ' })
             }
             println!();
         }
 
         panic!();
+    }
+}
+
+impl CathodeRayTube {
+    fn run<F: FnMut(i64, i64)>(&self, mut apply: F) {
+        let mut pause = false;
+        let mut cycle = 1;
+        let mut x = 1;
+
+        let mut iter = self.0.iter().peekable();
+
+        while let Some(ins) = iter.peek() {
+            apply(cycle, x);
+
+            match ins {
+                Instruction::Noop => {
+                    iter.next();
+                    cycle += 1;
+                }
+                Instruction::Addx(_) if !pause => {
+                    pause = true;
+                    cycle += 1;
+                }
+                Instruction::Addx(dx) => {
+                    x += dx;
+                    pause = false;
+                    iter.next();
+                    cycle += 1;
+                }
+            }
+        }
     }
 }
