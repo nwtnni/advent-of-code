6eed20de0ad84ccd5f44875fb52d5e8ba752508a
Newton Ni <nwtnni@gmail.com>
2022-12-13 23:30:54 -0600

    Refactor 2022 day 14

--- aoc-22/src/day_14.rs
+++ aoc-22/src/day_14.rs
@@ -1,56 +1,48 @@
 use std::cmp;
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 use aoc::*;
 
 #[derive(Clone, Debug)]
-pub struct RegolithReservoir(Vec<Vec<(i64, i64)>>);
+pub struct RegolithReservoir(HashSet<(i64, i64)>);
 
 impl Fro for RegolithReservoir {
     fn fro(input: &str) -> Self {
-        input
-            .trim()
-            .split('\n')
-            .map(|line| {
-                line.split(" -> ")
-                    .map(|xy| {
-                        let (x, y) = xy.split_once(',').unwrap();
-                        (i64::fro(x), i64::fro(y))
-                    })
-                    .collect()
-            })
-            .collect::<Vec<_>>()
-            .tap(Self)
-    }
-}
+        let mut grid = HashSet::new();
 
-#[derive(Debug)]
-enum Tile {
-    Wall,
-    Sand,
-}
+        for line in input.trim().split('\n') {
+            let points = line
+                .split(" -> ")
+                .map(|xy| {
+                    let (x, y) = xy.split_once(',').unwrap();
+                    (i64::fro(x), i64::fro(y))
+                })
+                .collect::<Vec<_>>();
 
-impl Solution for RegolithReservoir {
-    fn one(self) -> i64 {
-        let mut grid = HashMap::new();
-        for line in self.0 {
-            for points in line.windows(2) {
-                // x same, y different
-                if points[0].0 == points[1].0 {
-                    for y in cmp::min(points[0].1, points[1].1)..=cmp::max(points[0].1, points[1].1)
-                    {
-                        grid.insert((points[0].0, y), Tile::Wall);
-                    }
+            for window in points.windows(2) {
+                let (x1, y1) = window[0];
+                let (x2, y2) = window[1];
+                let points = if x1 == x2 {
+                    (cmp::min(y1, y2)..=cmp::max(y1, y2))
+                        .map(move |y| (x1, y))
+                        .tap(Or::L)
                 } else {
-                    for x in cmp::min(points[0].0, points[1].0)..=cmp::max(points[0].0, points[1].0)
-                    {
-                        grid.insert((x, points[0].1), Tile::Wall);
-                    }
-                }
+                    (cmp::min(x1, x2)..=cmp::max(x1, x2))
+                        .map(move |x| (x, y1))
+                        .tap(Or::R)
+                };
+
+                grid.extend(points);
             }
         }
 
-        let low = grid.keys().map(|(_, y)| *y).max().unwrap();
+        Self(grid)
+    }
+}
+
+impl Solution for RegolithReservoir {
+    fn one(mut self) -> i64 {
+        let void = self.0.iter().map(|(_, y)| *y).max().unwrap();
         let mut total = 0;
 
         loop {
@@ -58,47 +50,29 @@ impl Solution for RegolithReservoir {
             let mut y = 0;
 
             loop {
-                if y > low {
+                if y > void {
                     return total;
                 }
 
-                if grid.get(&(x, y + 1)).is_none() {
+                if self.0.get(&(x, y + 1)).is_none() {
                     y += 1;
-                } else if grid.get(&(x - 1, y + 1)).is_none() {
+                } else if self.0.get(&(x - 1, y + 1)).is_none() {
                     x -= 1;
                     y += 1;
-                } else if grid.get(&(x + 1, y + 1)).is_none() {
+                } else if self.0.get(&(x + 1, y + 1)).is_none() {
                     x += 1;
                     y += 1;
                 } else {
                     total += 1;
-                    grid.insert((x, y), Tile::Sand);
+                    self.0.insert((x, y));
                     break;
                 }
             }
         }
     }
 
-    fn two(self) -> i64 {
-        let mut grid = HashMap::new();
-        for line in self.0 {
-            for points in line.windows(2) {
-                // x same, y different
-                if points[0].0 == points[1].0 {
-                    for y in cmp::min(points[0].1, points[1].1)..=cmp::max(points[0].1, points[1].1)
-                    {
-                        grid.insert((points[0].0, y), Tile::Wall);
-                    }
-                } else {
-                    for x in cmp::min(points[0].0, points[1].0)..=cmp::max(points[0].0, points[1].0)
-                    {
-                        grid.insert((x, points[0].1), Tile::Wall);
-                    }
-                }
-            }
-        }
-
-        let low = grid.keys().map(|(_, y)| *y).max().unwrap();
+    fn two(mut self) -> i64 {
+        let floor = self.0.iter().map(|(_, y)| *y).max().unwrap();
         let mut total = 0;
 
         loop {
@@ -106,17 +80,19 @@ impl Solution for RegolithReservoir {
             let mut y = 0;
 
             loop {
-                if grid.get(&(x, y + 1)).is_none() && y + 1 < low + 2 {
+                let above = y + 1 < floor + 2;
+
+                if self.0.get(&(x, y + 1)).is_none() && above {
                     y += 1;
-                } else if grid.get(&(x - 1, y + 1)).is_none() && y + 1 < low + 2 {
+                } else if self.0.get(&(x - 1, y + 1)).is_none() && above {
                     x -= 1;
                     y += 1;
-                } else if grid.get(&(x + 1, y + 1)).is_none() && y + 1 < low + 2 {
+                } else if self.0.get(&(x + 1, y + 1)).is_none() && above {
                     x += 1;
                     y += 1;
                 } else {
                     total += 1;
-                    grid.insert((x, y), Tile::Sand);
+                    self.0.insert((x, y));
 
                     if x == 500 && y == 0 {
                         return total;
