1d3cd6c2989b56ea856b9c661fdc108707e91e7a
Newton Ni <nwtnni@gmail.com>
2022-12-18 15:24:35 -0600

    Refactor 2022 day 18

--- aoc-22/src/day_18.rs
+++ aoc-22/src/day_18.rs
@@ -27,69 +27,81 @@ impl Fro for BoilingBoulders {
 
 impl Solution for BoilingBoulders {
     fn one(self) -> i64 {
-        let mut faces = HashMap::new();
-
-        for (x, y, z) in self.0 {
-            let mut face = 0b0011_1111i64;
-
-            match faces.get_mut(&(x + 1, y, z)) {
-                None => (),
-                Some(other) => {
-                    face &= 0b0001_1111;
-                    *other &= 0b0010_1111;
-                }
-            }
-
-            match faces.get_mut(&(x - 1, y, z)) {
-                None => (),
-                Some(other) => {
-                    face &= 0b0010_1111;
-                    *other &= 0b0001_1111;
-                }
-            }
-
-            match faces.get_mut(&(x, y + 1, z)) {
-                None => (),
-                Some(other) => {
-                    face &= 0b0011_0111;
-                    *other &= 0b0011_1011;
-                }
-            }
-
-            match faces.get_mut(&(x, y - 1, z)) {
-                None => (),
-                Some(other) => {
-                    face &= 0b0011_1011;
-                    *other &= 0b0011_0111;
-                }
-            }
-
-            match faces.get_mut(&(x, y, z + 1)) {
-                None => (),
-                Some(other) => {
-                    face &= 0b0011_1101;
-                    *other &= 0b0011_1110;
-                }
-            }
-
-            match faces.get_mut(&(x, y, z - 1)) {
-                None => (),
-                Some(other) => {
-                    face &= 0b0011_1110;
-                    *other &= 0b0011_1101;
-                }
-            }
-
-            faces.insert((x, y, z), face);
-        }
-
-        faces.values().map(|face| (*face).count_ones()).sum::<u32>() as i64
+        self.faces()
+            .values()
+            .map(|face| face.count_ones())
+            .sum::<u32>() as i64
     }
 
     fn two(self) -> i64 {
+        let faces = self.faces();
+        let (min @ (min_x, min_y, min_z), (max_x, max_y, max_z)) = self.bounds();
+
+        let mut total = 0;
+        let mut stack = Vec::new();
+        let mut seen = HashSet::new();
+
+        stack.push(min);
+
+        while let Some((x, y, z)) = stack.pop() {
+            if x < min_x
+                || y < min_y
+                || z < min_z
+                || x > max_x
+                || y > max_y
+                || z > max_z
+                || faces.contains_key(&(x, y, z))
+                || !seen.insert((x, y, z))
+            {
+                continue;
+            }
+
+            match faces.get(&(x + 1, y, z)) {
+                Some(face) if face & 0b0001_0000 > 0 => total += 1,
+                Some(_) => (),
+                None => stack.push((x + 1, y, z)),
+            }
+
+            match faces.get(&(x - 1, y, z)) {
+                Some(face) if face & 0b0010_0000 > 0 => total += 1,
+                Some(_) => (),
+                None => stack.push((x - 1, y, z)),
+            }
+
+            match faces.get(&(x, y + 1, z)) {
+                Some(face) if face & 0b0000_0100 > 0 => total += 1,
+                Some(_) => (),
+                None => stack.push((x, y + 1, z)),
+            }
+
+            match faces.get(&(x, y - 1, z)) {
+                Some(face) if face & 0b0000_1000 > 0 => total += 1,
+                Some(_) => (),
+                None => stack.push((x, y - 1, z)),
+            }
+
+            match faces.get(&(x, y, z + 1)) {
+                Some(face) if face & 0b0000_0001 > 0 => total += 1,
+                Some(_) => (),
+                None => stack.push((x, y, z + 1)),
+            }
+
+            match faces.get(&(x, y, z - 1)) {
+                Some(face) if face & 0b0000_0010 > 0 => total += 1,
+                Some(_) => (),
+                None => stack.push((x, y, z - 1)),
+            }
+        }
+
+        total
+    }
+}
+
+impl BoilingBoulders {
+    fn faces(&self) -> HashMap<(i64, i64, i64), u8> {
         let mut faces = HashMap::new();
 
-        for (x, y, z) in self.0 {
+        for (x, y, z) in self.0.iter().copied() {
             let mut face = 0b0011_1111;
 
             match faces.get_mut(&(x + 1, y, z)) {
@@ -144,7 +156,10 @@ impl Solution for BoilingBoulders {
         }
 
         faces.retain(|_, face| *face > 0);
+        faces
+    }
 
+    fn bounds(&self) -> ((i64, i64, i64), (i64, i64, i64)) {
         let mut min_x = i64::MAX;
         let mut max_x = i64::MIN;
 
@@ -154,7 +169,7 @@ impl Solution for BoilingBoulders {
         let mut min_z = i64::MAX;
         let mut max_z = i64::MIN;
 
-        for (x, y, z) in faces.keys().copied() {
+        for (x, y, z) in &self.0 {
             min_x = cmp::min(x - 1, min_x);
             max_x = cmp::max(x + 1, max_x);
 
@@ -165,62 +180,6 @@ impl Solution for BoilingBoulders {
             max_z = cmp::max(z + 1, max_z);
         }
 
-        let mut total = 0;
-        let mut stack = Vec::new();
-        let mut seen = HashSet::new();
-
-        stack.push((min_x, min_y, min_z));
-
-        while let Some((x, y, z)) = stack.pop() {
-            if x < min_x
-                || y < min_y
-                || z < min_z
-                || x > max_x
-                || y > max_y
-                || z > max_z
-                || faces.contains_key(&(x, y, z))
-                || !seen.insert((x, y, z))
-            {
-                continue;
-            }
-
-            match faces.get(&(x + 1, y, z)) {
-                Some(face) if face & 0b0001_0000 > 0 => total += 1,
-                Some(_) => (),
-                None => stack.push((x + 1, y, z)),
-            }
-
-            match faces.get(&(x - 1, y, z)) {
-                Some(face) if face & 0b0010_0000 > 0 => total += 1,
-                Some(_) => (),
-                None => stack.push((x - 1, y, z)),
-            }
-
-            match faces.get(&(x, y + 1, z)) {
-                Some(face) if face & 0b0000_0100 > 0 => total += 1,
-                Some(_) => (),
-                None => stack.push((x, y + 1, z)),
-            }
-
-            match faces.get(&(x, y - 1, z)) {
-                Some(face) if face & 0b0000_1000 > 0 => total += 1,
-                Some(_) => (),
-                None => stack.push((x, y - 1, z)),
-            }
-
-            match faces.get(&(x, y, z + 1)) {
-                Some(face) if face & 0b0000_0001 > 0 => total += 1,
-                Some(_) => (),
-                None => stack.push((x, y, z + 1)),
-            }
-
-            match faces.get(&(x, y, z - 1)) {
-                Some(face) if face & 0b0000_0010 > 0 => total += 1,
-                Some(_) => (),
-                None => stack.push((x, y, z - 1)),
-            }
-        }
-
-        total
+        ((min_x, min_y, min_z), (max_x, max_y, max_z))
     }
 }
