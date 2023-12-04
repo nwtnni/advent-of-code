d01c56c142d3d2af969862201461f0c43464810a
Newton Ni <nwtnni@gmail.com>
2023-12-04 11:18:17 -0600

    Clean up 2023 day 3

--- aoc-23/src/day_03.rs
+++ aoc-23/src/day_03.rs
@@ -6,94 +6,72 @@ use aoc::*;
 #[derive(Clone, Debug)]
 pub struct GearRatios {
     symbols: HashMap<(usize, usize), u8>,
-    digits: HashMap<(usize, usize), String>,
-    original: HashMap<(usize, usize), String>,
+    unique: HashMap<(usize, usize), String>,
+    all: HashMap<(usize, usize), (usize, usize, String)>,
 }
 
 impl Fro for GearRatios {
     fn fro(input: &str) -> Self {
         let mut symbols = HashMap::new();
-        let mut digits = HashMap::new();
-        input
-            .trim()
-            .split('\n')
-            .enumerate()
-            .for_each(|(line_index, line)| {
-                let mut i = 0;
-                while i < line.as_bytes().len() {
-                    match line.as_bytes()[i] {
-                        b'.' => i += 1,
-                        b'0'..=b'9' => {
-                            let mut j = i + 1;
-                            while j <= line.as_bytes().len() && line[i..j].parse::<i64>().is_ok() {
-                                j += 1;
-                            }
-                            digits.insert((line_index, i), dbg!(String::from(&line[i..j - 1])));
-                            i += j - 1 - i;
-                        }
-                        other => {
-                            dbg!(((line_index, i), other));
-                            symbols.insert((line_index, i), other);
-                            i += 1;
-                        }
-                    }
-                }
-            });
+        let mut unique = HashMap::new();
 
-        let mut original = HashMap::new();
-        input
-            .trim()
-            .split('\n')
-            .enumerate()
-            .for_each(|(line_index, line)| {
-                let mut i = 0;
-                while i < line.as_bytes().len() {
-                    match line.as_bytes()[i] {
-                        b'0'..=b'9' => {
-                            let mut j = i + 1;
-                            while j <= line.as_bytes().len() && line[i..j].parse::<i64>().is_ok() {
-                                j += 1;
+        input.trim().split('\n').enumerate().for_each(|(i, line)| {
+            let mut j = 0;
+            while j < line.len() {
+                match line.as_bytes()[j] {
+                    b'.' => j += 1,
+                    b'0'..=b'9' => {
+                        let mut dj = 1;
+
+                        loop {
+                            match j + dj + 1 > line.len()
+                                || line[j..][..dj + 1].parse::<i64>().is_err()
+                            {
+                                true => break,
+                                false => dj += 1,
                             }
-                            let digit = String::from(&line[i..j - 1]);
-                            for j in i..j - 1 {
-                                dbg!((line_index, j), &digit);
-                                original.insert((line_index, j), digit.clone());
-                            }
-                            i += j - 1 - i;
                         }
-                        _ => {
-                            i += 1;
-                        }
+
+                        unique.insert((i, j), String::from(&line[j..][..dj]));
+                        j += dj;
+                    }
+                    other => {
+                        symbols.insert((i, j), other);
+                        j += 1;
                     }
                 }
-            });
+            }
+        });
+
+        let mut all = HashMap::new();
+        for ((i, j), number) in unique.iter() {
+            for dj in 0..number.len() {
+                all.insert((*i, j + dj), (*i, *j, number.clone()));
+            }
+        }
 
         Self {
             symbols,
-            digits,
-            original,
+            unique,
+            all,
         }
     }
 }
 
 impl Solution for GearRatios {
     fn one(self) -> i64 {
-        self.digits
+        self.unique
             .iter()
             .filter(|((i, j), n)| {
-                if j.checked_sub(1)
-                    .filter(|j| self.symbols.contains_key(&(*i, *j)))
-                    .is_some()
-                    || self.symbols.contains_key(&(*i, j + n.as_bytes().len()))
+                if self.symbols.contains_key(&(*i, j.wrapping_sub(1)))
+                    || self.symbols.contains_key(&(*i, j + n.len()))
                 {
                     dbg!(n);
                     return true;
                 }
 
-                for j in j.saturating_sub(1)..j + n.as_bytes().len() + 1 {
-                    if i.checked_sub(1)
-                        .filter(|i| self.symbols.contains_key(&(*i, j)))
-                        .is_some()
+                for j in j.saturating_sub(1)..=j + n.len() {
+                    if self.symbols.contains_key(&(i.wrapping_sub(1), j))
                         || self.symbols.contains_key(&(i + 1, j))
                     {
                         dbg!(n);
@@ -101,7 +79,6 @@ impl Solution for GearRatios {
                     }
                 }
 
-                dbg!(n);
                 false
             })
             .map(|(_, n)| n.parse::<i64>().unwrap())
@@ -116,36 +93,31 @@ impl Solution for GearRatios {
                     return None;
                 }
 
-                let lu = self.original.get(&(i.wrapping_sub(1), j.wrapping_sub(1)));
-                let ld = self.original.get(&(i.wrapping_sub(1), j.wrapping_add(1)));
-                let ru = self.original.get(&(i.wrapping_add(1), j.wrapping_sub(1)));
-                let rd = self.original.get(&(i.wrapping_add(1), j.wrapping_add(1)));
+                let l = self.all.get(&(i.wrapping_sub(1), *j));
+                let r = self.all.get(&(i.wrapping_add(1), *j));
+                let u = self.all.get(&(*i, j.wrapping_sub(1)));
+                let d = self.all.get(&(*i, j.wrapping_add(1)));
 
-                let l = self.original.get(&(i.wrapping_sub(1), *j));
-                let r = self.original.get(&(i.wrapping_add(1), *j));
-                let u = self.original.get(&(*i, j.wrapping_sub(1)));
-                let d = self.original.get(&(*i, j.wrapping_add(1)));
+                let lu = self.all.get(&(i.wrapping_sub(1), j.wrapping_sub(1)));
+                let ld = self.all.get(&(i.wrapping_sub(1), j.wrapping_add(1)));
+                let ru = self.all.get(&(i.wrapping_add(1), j.wrapping_sub(1)));
+                let rd = self.all.get(&(i.wrapping_add(1), j.wrapping_add(1)));
 
-                if [l, r, u, d, lu, ld, ru, rd]
+                let around = [l, r, u, d, lu, ld, ru, rd]
                     .into_iter()
                     .flatten()
-                    .collect::<HashSet<_>>()
-                    .len()
-                    == 2
-                {
-                    dbg!((i, j));
-                    Some(
-                        [l, r, u, d, lu, ld, ru, rd]
-                            .into_iter()
-                            .flatten()
-                            .collect::<HashSet<_>>()
-                            .into_iter()
-                            .map(|n| n.parse::<i64>().unwrap())
-                            .product::<i64>(),
-                    )
-                } else {
-                    None
+                    .collect::<HashSet<_>>();
+
+                if around.len() != 2 {
+                    return None;
                 }
+
+                Some(
+                    around
+                        .into_iter()
+                        .map(|(_, _, n)| n.parse::<i64>().unwrap())
+                        .product::<i64>(),
+                )
             })
             .sum()
     }
