7e590226c3f11da6de674a6e5c552327c4182fc8
Newton Ni <nwtnni@gmail.com>
2023-12-11 00:03:38 -0600

    Implement 2023 day 10

--- aoc-23/src/day_10.rs
+++ aoc-23/src/day_10.rs
@@ -6,6 +6,8 @@ use aoc::*;
 #[derive(Clone, Debug)]
 pub struct PipeMaze {
     start: Pos,
+    rows: i64,
+    cols: i64,
     grid: HashMap<Pos, Pipe>,
 }
 
@@ -22,8 +24,15 @@ impl Fro for PipeMaze {
     fn fro(input: &str) -> Self {
         let mut grid = HashMap::new();
         let mut start = Pos::default();
+        let mut rows = 0;
+        let mut cols = 0;
         for (y, row) in input.trim().split('\n').enumerate() {
+            rows += 1;
+            cols = 0;
+
             for (x, col) in row.trim().chars().enumerate() {
+                cols += 1;
+
                 let pos = Pos {
                     x: x as i64,
                     y: y as i64,
@@ -47,79 +56,105 @@ impl Fro for PipeMaze {
                 grid.insert(pos, cell);
             }
         }
-        Self { start, grid }
+        Self {
+            start,
+            rows,
+            cols,
+            grid,
+        }
     }
 }
 
 impl Solution for PipeMaze {
     fn one(mut self) -> i64 {
-        let mut visited = HashSet::new();
         for pipe in PIPES {
-            visited.clear();
-            visited.insert(self.start);
             self.grid.insert(self.start, pipe);
 
-            if let Some(distance) = self.search(&mut visited, self.start, self.start, 0) {
-                return (distance + 1) / 2;
+            if let Some(pipes) = self.trace(self.start) {
+                return (pipes.len() as i64 + 1) / 2;
             }
         }
 
         unreachable!()
     }
 
-    fn two(self) -> i64 {
-        todo!()
+    fn two(mut self) -> i64 {
+        for pipe in PIPES {
+            self.grid.insert(self.start, pipe);
+
+            if let Some(pipes) = self.trace(self.start) {
+                let mut total = 0;
+                for i in 0..self.rows {
+                    let mut inside = false;
+
+                    for j in 0..self.cols {
+                        match pipes.get(&Pos { x: j, y: i }) {
+                            None => total += inside as i64,
+                            Some(pos) => {
+                                inside ^= self.grid[pos] & N > 0;
+                            }
+                        }
+                    }
+                }
+
+                return total;
+            }
+        }
+
+        unreachable!()
     }
 }
 
 impl PipeMaze {
-    fn search(
-        &self,
-        visited: &mut HashSet<Pos>,
-        start: Pos,
-        end: Pos,
-        distance: i64,
-    ) -> Option<i64> {
-        let pipe = *self.grid.get(&start)?;
+    fn trace(&self, start: Pos) -> Option<HashSet<Pos>> {
+        let mut visited = HashSet::new();
+        visited.insert(start);
 
-        for next in [
-            if pipe & N > 0 {
-                Some(start.shift(Dir::N))
-            } else {
-                None
-            },
-            if pipe & S > 0 {
-                Some(start.shift(Dir::S))
-            } else {
-                None
-            },
-            if pipe & E > 0 {
-                Some(start.shift(Dir::E))
-            } else {
-                None
-            },
-            if pipe & W > 0 {
-                Some(start.shift(Dir::W))
-            } else {
-                None
-            },
-        ]
-        .into_iter()
-        .flatten()
-        {
-            if next == end && distance > 1 {
-                return Some(distance);
+        let mut here = start;
+
+        'outer: loop {
+            let pipe = *self.grid.get(&here)?;
+
+            'inner: for (next, _) in [
+                if pipe & N > 0 {
+                    Some((here.shift(Dir::N), S))
+                } else {
+                    None
+                },
+                if pipe & S > 0 {
+                    Some((here.shift(Dir::S), N))
+                } else {
+                    None
+                },
+                if pipe & E > 0 {
+                    Some((here.shift(Dir::E), W))
+                } else {
+                    None
+                },
+                if pipe & W > 0 {
+                    Some((here.shift(Dir::W), E))
+                } else {
+                    None
+                },
+            ]
+            .into_iter()
+            .flatten()
+            .filter(|(next, _)| self.grid.contains_key(next))
+            .filter(|(next, opposite)| self.grid[next] & opposite > 0)
+            {
+                if visited.len() > 2 && next == start {
+                    return Some(visited);
+                }
+
+                if visited.insert(next) {
+                    here = next;
+                    continue 'outer;
+                } else {
+                    continue 'inner;
+                }
             }
 
-            if !visited.insert(next) {
-                continue;
-            }
-
-            if let Some(distance) = self.search(visited, next, end, distance + 1) {
-                return Some(distance);
-            }
+            return None;
         }
-
-        None
     }
 }
